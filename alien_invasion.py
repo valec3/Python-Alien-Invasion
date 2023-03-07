e326833358618b3abc5e18b28968405f13d23803
Victor Raul <v1c70rotakmer@gmail.com>
2023-03-06 22:20:37 -0500

    Comenzar modularizacion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,19 @@
 import sys 
 
 import pygame
-
+from studs import Settings
 class AlienInvasion:
     """Clase general para gestionar los activos y el comportamiento del juego."""
     def __init__(self):
         """Inicializar el juego, y crear los recursos"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen=pygame.display.set_mode((1200,800))
+        self.settings=Settings()
+        self.screen=pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
-        # Set the background color.
+        # Establecer el color de fondo.
         self.bg_color = (230, 230, 230)
         
     def run_game(self):
@@ -20,6 +23,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            #Redibujar la pantalla durante cada paso del bucle
+            self.screen.fill(self.settings.bg_color)
             #Hacer visible la pantalla dibijada mas recientemente
             pygame.display.flip()
             self.clock.tick(60)
