b4a1cf2f317b6ec971561f1f7eebcec964959a18
Victor Raul <v1c70rotakmer@gmail.com>
2023-03-07 06:50:36 -0500

    Ultimos cambios de esta semana

--- studs.py
+++ studs.py
@@ -7,3 +7,8 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed=2
+        # Bullet settings
+        self.bullet_speed = 3.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (90, 60, 60)
