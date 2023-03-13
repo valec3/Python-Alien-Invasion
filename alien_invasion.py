4be7b356aabfa6334eabe3fa23249a950f27167b
Victor Raul <v1c70rotakmer@gmail.com>
2023-03-13 18:49:31 -0500

    Probar disparos de la nave

--- alien_invasion.py
+++ alien_invasion.py
@@ -47,6 +47,8 @@ class AlienInvasion:
                 self.ship.moving_left=True
             elif event.key ==pygame.K_q:
                 sys.exit()
+            elif event.key == pygame.K_SPACE:
+                self._fire_bullet()
     
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -56,10 +58,17 @@ class AlienInvasion:
             #mover la nave a la izquierda
             self.ship.moving_left =False
     
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+    
     def _update_screen(self):
         """Actualizar imagenes en la pantalla, y voltear a la nueva."""
         self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()#dibujar nave
         pygame.display.flip()
 
 if __name__ =="__main__":
