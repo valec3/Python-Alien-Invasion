4476a6122ee05932471ac2030ae25006c30bda36
Victor Raul <v1c70rotakmer@gmail.com>
2023-03-13 20:36:16 -0500

    Crear primer alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ import pygame
 from studs import Settings
 from ship import Ship
 from blet import Bullet
+from alien import Alien
+
 class AlienInvasion:
     """Clase general para gestionar los activos y el comportamiento del juego."""
     def __init__(self):
@@ -10,6 +12,7 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings=Settings()
+        #Establecer el tamaño de la pantalla
         self.screen=pygame.display.set_mode(
             (self.settings.screen_width,self.settings.screen_height)
         )
@@ -17,6 +20,8 @@ class AlienInvasion:
 
         self.ship =Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
         
     def run_game(self):
         """Comenzar el bucle principal para el juego"""
@@ -25,12 +30,12 @@ class AlienInvasion:
             self.ship.update()#actualiza la posicion de la nave
             self._update_bullets()#actualiza la posicion de las balas
             self._update_screen()#dibujasmos una nueva pantalla con las posiciones actuales
-            self.clock.tick(60)
+            self.clock.tick(60)#Velocidad de fotogramas por segundo
             
     def _check_events(self):
         """Reponde a las teclas presionadas y eventos del mouse"""
         #Ver eventos del teclado
-        for event in pygame.event.get():
+        for event in pygame.event.get():#recoge cada evento
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type==pygame.KEYDOWN:
@@ -39,6 +44,7 @@ class AlienInvasion:
                 self._check_keyup_events(event)
                     
     def _check_keydown_events(self, event):
+            """Detecta las teclas PRESIONADAS"""
             if event.key == pygame.K_RIGHT:
                 #mover la nave a la derecha
                 self.ship.moving_right=True
@@ -51,6 +57,7 @@ class AlienInvasion:
                 self._fire_bullet()
     
     def _check_keyup_events(self, event):
+        """Detecta las teclas SOLTADAS luego de ser presionadas"""
         if event.key == pygame.K_RIGHT:
             #mover la nave a la derecha
                 self.ship.moving_right=False
@@ -74,14 +81,19 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-    
+    def _create_fleet(self):
+        """Crear la flota de aliens"""
+        # Hacer un alien.
+        alien = Alien(self)
+        self.aliens.add(alien)        
     def _update_screen(self):
         """Actualizar imagenes en la pantalla, y voltear a la nueva."""
-        self.screen.fill(self.settings.bg_color)
+        self.screen.fill(self.settings.bg_color)#color de fondo
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()#dibujar nave
-        pygame.display.flip()
+        self.aliens.draw(self.screen) #dibujar alien
+        pygame.display.flip()#hacer visible la pantalla mas actual
 
 if __name__ =="__main__":
     #Hacer una instancia, y correr el juego
